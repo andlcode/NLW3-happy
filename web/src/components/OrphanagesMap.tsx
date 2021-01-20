0d137a91ce434c4441013af66b8e1a89c69461a5
Andre Luiz D C <and969696@outlook.com>
2021-01-20 16:38:26 -0300

    fix: mapbox

--- web/src/components/OrphanagesMap.tsx
+++ web/src/components/OrphanagesMap.tsx
@@ -27,12 +27,12 @@ style={{ width: '100%', height: '100%'}}
 >
 
 
-        
+{/*         
         <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"></TileLayer>
-        
-        {/* <TileLayer
+         */}
+         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-        /> */}
+        /> 
         </MapContainer>
         <Link to="/" className="create-orphanage">
             <FiPlus size={32} color="#FFF" />
