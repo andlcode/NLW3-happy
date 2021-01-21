f5cd08e6bce2a8ccfce235f397ecf920d30dd46c
Andre Luiz D C <and969696@outlook.com>
2021-01-21 13:47:37 -0300

    add connection with database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,11 @@
+import './database/connection';
+
 import express from 'express';
 
 const app = express();
+
+app.get('/users', (request, response) => {
+   return response.json({messege: 'Hello World'});
+});
 
 app.listen(3333);
